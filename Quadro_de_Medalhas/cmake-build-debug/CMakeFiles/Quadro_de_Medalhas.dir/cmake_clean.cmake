13bf0f60f56f0f6a4dd8cf9f99fdbc726956e5cf
Rafael de Castro <rafael.castrofreitas27@gmail.com>
2020-11-07 20:18:45 -0300

    Initial Commit

---
+++ Quadro_de_Medalhas/cmake-build-debug/CMakeFiles/Quadro_de_Medalhas.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Quadro_de_Medalhas.dir/main.cpp.o"
+  "Quadro_de_Medalhas"
+  "Quadro_de_Medalhas.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Quadro_de_Medalhas.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Soma_Dois_Quadrados/cmake-build-debug/CMakeFiles/Soma_Dois_Quadrados.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Soma_Dois_Quadrados.dir/main.cpp.o"
+  "Soma_Dois_Quadrados"
+  "Soma_Dois_Quadrados.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Soma_Dois_Quadrados.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
