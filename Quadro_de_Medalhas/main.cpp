1a9cb6415fcfad45458a6672fd3dc85c092fdc46
Rafael de Castro <rafael.castrofreitas27@gmail.com>
2020-11-14 16:17:15 -0300

    Estruturando o problema Quadro de Medalhas

--- Quadro_de_Medalhas/main.cpp
+++ Quadro_de_Medalhas/main.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
 using namespace std;
 
+typedef struct {
+    char nome[10];
+    int medalha_ouro;
+    int medalha_prata;
+    int medalha_bronze;
+
+}QuadroPaises;
+
+void ordenaPaises(QuadroPaises p, int n){
+
+}
+
 int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
+    int num_paises;
+
+
+    cin >> num_paises;
+    QuadroPaises paises[num_paises];
+
+    for (int i = 0; i < num_paises; i++){
+        cin >> paises[i].nome;
+        cin >> paises[i].medalha_ouro;
+        cin >> paises[i].medalha_prata;
+        cin >> paises[i].medalha_bronze;
+    }
+
+    /*cout << "\n========== Quadro de países ==========";
+    for (int i = 0; i < num_paises; i++){
+        cout << "\nNome: " << paises[i].nome << "\nMedalhas de ouro: " << paises[i].medalha_ouro <<
+                ", Medalhas de prata: " << paises[i].medalha_prata <<
+                ", Medalhas de bronze: " << paises[i].medalha_bronze << "\n";
+    }*/
 }
